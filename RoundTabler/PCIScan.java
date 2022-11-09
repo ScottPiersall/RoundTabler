1dd789d3e378c28469aa645dda0da4fb3731f879
Scott Piersall <spiersall@gmail.com>
2022-11-09 18:28:48 -0500

    Added Unit Tests For Confidence Level Calculations for PCIScan

--- RoundTabler/PCIScan.java
+++ RoundTabler/PCIScan.java
@@ -14,6 +14,11 @@ public class PCIScan{
 	static String CardNumberSequenceRegex = "\b(?:\\d[-]*?){13,16}\b";
 	static Pattern CardNumberPattern = Pattern.compile( CardNumberSequenceRegex);
 
+
+	public PCIScan(){
+		super();
+	}
+
 	public static int getConfidenceLevelmatch( String DatabaseRow ) {
 		int result = 0;
 		
@@ -29,11 +34,28 @@ public class PCIScan{
 				result += 25;
 			}
 
-
-
-
 		return result;
 	}
 
+	public void TestConfidence_100(){
+		assert (PCIScan.getConfidenceLevelmatch("The customer's card number is 6011-1111-1111-1117 and they said on the phone we could sign them up for auto pay" )) == 100;
+	}
+
+	public void TestConfidence_75(){
+		assert (PCIScan.getConfidenceLevelmatch("The number is 6011121511111117, which looks like a card but fails Luhn's test' " )) == 75;
+	}
+
+	public void TestConfidence_0(){
+		assert (PCIScan.getConfidenceLevelmatch("There is no card data in this field" )) == 0;
+		assert (PCIScan.getConfidenceLevelmatch("")) == 0;
+	}
+
+	public static void main(String[] args){
+			PCIScan Tests;
+			Tests = new PCIScan();
+			Tests.TestConfidence_0();
+			Tests.TestConfidence_75();
+			Tests.TestConfidence_100();
+	}
 
 }
