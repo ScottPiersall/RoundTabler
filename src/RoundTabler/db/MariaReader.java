e25f7ffb9b9dcbe098978c2f24d895cb0455f73a
Jordan <jmcmillan@knights.ucf.edu>
2022-11-14 18:30:52 -0500

    Update each reader type for new DBReader class

--- src/RoundTabler/db/MariaReader.java
+++ src/RoundTabler/db/MariaReader.java
@@ -2,7 +2,7 @@ package RoundTabler.db;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import RoundTabler.Configuration;
 
@@ -13,6 +13,8 @@ import RoundTabler.Configuration;
 
 public class MariaReader extends DBReader {
 
+    PreparedStatement buildSchema = null;
+
     public MariaReader(Configuration config) throws SQLException {
         super(config);
 
@@ -22,32 +24,49 @@ public class MariaReader extends DBReader {
                
         // Use args to establish database connection
         String jdbcUri = String.format("jdbc:mariadb://%s?user=%s&password=%s",
-                                                  config.getServer(),
-                                                   config.getUser(), config.getPassword());
+                                                config.getServer(),
+                                                config.getUser(), config.getPassword());
 
         this.conn = DriverManager.getConnection(jdbcUri);
-        
+
+        // Create the relevant query string for forming populating our SchemaItems
+        // SELECT TABLE_NAME, COLUMN_NAME from information_schema.COLUMNS where table_schema=DATABASE() and TABLE_NAME =" + config.getTable() + "and DATA_TYPE in (mediumtext, longtext, text, tinytext, varchar);"
+        StringBuilder stmtString = new StringBuilder();
+        stmtString.append("SELECT TABLE_NAME, COLUMN_NAME, DATA_TYPE FROM information_schema.COLUMNS WHERE DATA_TYPE IN ");
+        stmtString.append("('mediumtext', 'longtext', 'text', 'tinytext', 'varchar') ");
+        stmtString.append("AND TABLE_SCHEMA=?");
+        if (!config.getTable().isBlank()) { stmtString.append("AND TABLE_NAME=?"); }
+
+        buildSchema = conn.prepareStatement(stmtString.toString());
+        buildSchema.setString(1, config.getDatabase()); 
+        if (!config.getTable().isBlank()) { buildSchema.setString(2, config.getTable()); }
     }
 
-    public void getTables() {}
-
-    // Test connection for demonstration
-    // Gets the table, column, and privileges for columns
-    // TODO: refine the sql statement to not use %'s: just used as a proof of concept
-    public ResultSet getColumns() {
+    // Reads the schema and stores its information in the SchemaItems structure
+    public Boolean readSchema() {
         ResultSet rs = null;
 
+        // If we have already read the schema with this reader, then disallow future reads
+        if (!schemaItems.isEmpty())
+            return false;
+
         try {
-            Statement stmt = this.conn.createStatement();
-            rs = stmt.executeQuery("SELECT TABLE_NAME, COLUMN_NAME, PRIVILEGES FROM information_schema.COLUMNS WHERE COLUMN_TYPE LIKE 'varchar%' OR COLUMN_TYPE LIKE '%text%' AND PRIVILEGES like '%elect%'");
+            rs = buildSchema.executeQuery();
+
+            // If rs successfully produced results, then we iterate through it
+            if (rs != null) {
+                while (rs.next()) {
+                    // Table_Name, Column_Name, Data_Type
+                    schemaItems.Add(rs.getString(1), rs.getString(2), rs.getString(3));
+                }
+            }
         }
         catch (SQLException e) {
             System.out.println("Error in SQL execution: " + e);
+            return false;
         }
 
-        return rs;
+        return true;
     }
-
-    public void getColumnInfo() {}
 
 }
