145657177e737bea5bd9da8952c441c4cdd0e4d3
Jordan <jmcmillan@knights.ucf.edu>
2022-11-14 13:38:31 -0500

    Adjust DBReaders for homogeniety and add ReaderMaker

---
+++ src/RoundTabler/db/ReaderMaker.java
@@ -0,0 +1,48 @@
+package RoundTabler.db;
+
+import RoundTabler.Configuration;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.InputMismatchException;
+
+// DBReader factory
+public class ReaderMaker extends DBReader {
+    DBReader reader = null;
+
+    // When constructed, uses the configuration to create the proper DBReader type
+    public ReaderMaker(Configuration config) throws ClassNotFoundException, SQLException, InputMismatchException {
+        super(config);
+
+        switch (config.getDbType().toLowerCase()) {
+            case "mysql":
+                this.reader = new MySQLReader(config);
+                break;
+            case "mariadb":
+                this.reader = new MariaReader(config);
+                break;
+            case "mongo":
+            case "mongodb":
+                this.reader = new MongoReader(config);
+                break;
+            default:
+                // No accepted database provided, throw InputMismatchException
+                throw new InputMismatchException(String.format("Database type of %s not recognized or not supported; cannot instantiate DBReader.",
+                                                               config.getDbType()));
+        }
+    }
+
+    // Return the reader created by this
+    public DBReader getReader() {
+        return this.reader;
+    }
+
+    // Must cover the bases for an abstract class, these are not intended to be called
+    protected void executeQuery() {}
+
+    protected void getTables() {}
+
+    protected ResultSet getColumns() { return null; }
+
+    protected void getColumnInfo() {}
+}
