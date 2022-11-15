136cc9d3bc29c34c6597a8d12856a42cedc635dd
Jordan <jmcmillan@knights.ucf.edu>
2022-11-15 16:27:51 -0500

    Establish MongoDB Connection

--- src/RoundTabler/db/MongoReader.java
+++ src/RoundTabler/db/MongoReader.java
@@ -1,5 +1,12 @@
 package RoundTabler.db;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+import java.sql.SQLException;
 import RoundTabler.Configuration;
 
 /*
@@ -9,22 +16,32 @@ import RoundTabler.Configuration;
 
 public class MongoReader extends DBReader {
 
-    public MongoReader(Configuration config) throws ClassNotFoundException {
+    private MongoClient conn = null;
+
+    public MongoReader(Configuration config) throws ClassNotFoundException, SQLException {
         super(config);
 
         // Check for Java driver; if fails, throw ClassNotFoundException
         Class.forName("com.mongodb.client");
 
         // Use args to establish database connection
-        String dbUri = String.format("mongodb://%s:%s@%s:%s/%s",
+        String dbUri = String.format("mongodb://%s:%s@%s:%s",
                                     config.getUser(), config.getPassword(),
-                                    config.getServer(), !config.getPort().isBlank() ? config.getPort() : "27017",
-                                    config.getDatabase());
+                                    config.getServer(), !config.getPort().isBlank() ? config.getPort() : "27017");
 
-        
+        // The SQLException is fake here, but is done this way to ensure consistency across readers
+        try {
+            conn = MongoClients.create(dbUri);
+        }
+        catch (MongoException me) {
+            // Convert the MongoException to an SQLException to be handled later
+            throw new SQLException(me);
+        }
     }
 
-    public Boolean readSchema() { return false; }
+    public Boolean readSchema() { 
+        return false; 
+    }
 
     public java.util.ArrayList<String> readColumn(SchemaItem item) { return null; }
 }
