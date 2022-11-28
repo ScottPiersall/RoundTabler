d18707ff63cdeb54362e1ef40822ae6d545ba54d
Scott Piersall <spiersall@gmail.com>
2022-11-28 12:54:20 -0500

    Added Card Type identification function to PCI Scan Object

--- src/RoundTabler/PCIScan.java
+++ src/RoundTabler/PCIScan.java
@@ -27,6 +27,12 @@ public class PCIScan {
 	private int pLastMatchEnd;
 	private String pLastMatchDescription;
 
+
+	public enum CardType {
+			Amex, Discover, MasterCard, Visa, Undetermined
+	}
+
+
 	public PCIScan(){
 		super();
 		this.psbResults = new StringBuilder();
@@ -45,6 +51,62 @@ public class PCIScan {
 		return pLastMatchDescription;
 	}
 
+
+
+
+	 //
+	 // Increate confidence level by applying additional 
+	 // card number rules to determine exact card type
+	 // Based on BIN and length
+	 // 
+	 //  Card Type			Prefix			Card Number Length
+	 //
+	 // AMEX				34, 37			15
+	 // DISCOVER			6				16
+	 // MASTER CARD         51 to 55		16
+	 // VISA				4				13 or 16
+     private CardType GetCardType( String matchString ){
+		CardType Result;
+		int CardLength;
+		Result = Undetermined;
+		CardLength = matchString.length();
+
+		String firstChar;
+		firstchar = matchString(0,1);
+
+		select (firstChar){
+
+			case "3" : {
+				String firstTwo;
+				firstTwo = matchString(0,2);
+				if ( CardLength == 15 ) {
+						select (firstTwo ) {
+							case "34": return Amex;
+							case "37" : return Amex;
+						} 
+				}
+			}
+
+			case "4" : if (( CardLength == 13) || ( CardLength == 16 ) ) return Visa;
+			case "5" : if ( CardLength == 16 ) return MasterCard;
+			case "6" : if ( CardLength == 16 ) return Discover;
+
+		}
+
+
+		return Result;
+	 }
+
+
+	// Confidence level rules:
+	// CardNumberSequenceMatcher: 50%
+	// --------------------------------------------------------
+	// 50 % <--  Matches string of digits of appropriate length
+	// 75 % <-- 50% + Passes Luhn's test
+	// 100% <-- 50% + 75% + We can identify the card type
+	//
+	// CardPartialSequenceMatcher: 100%
+	//
 	public int getConfidenceLevelMatch(String DatabaseRow ) {
 		int result = 0;
 		
@@ -53,7 +115,7 @@ public class PCIScan {
 		// If we find what looks like a card sequence, make the confidence 75
 		if (CardNumberSequenceMatcher.find()) {
 
-			result += 75;   // Assign a confidence Level of at least 75%
+			result += 50;   // Assign a confidence Level of at least 75%
 			pLastMatchStart = CardNumberSequenceMatcher.start();
 			pLastMatchEnd = CardNumberSequenceMatcher.end();
 			// If the match passes LuhnsTest, Boost Confidence to 100
@@ -63,6 +125,24 @@ public class PCIScan {
 				pLastMatchDescription = pLastMatchDescription + "<BR>Luhn's Test";
 				pLastMatchStart = CardNumberSequenceMatcher.start();
 				pLastMatchEnd = CardNumberSequenceMatcher.end();
+
+				CardType thisCard;
+				thisCard = GetCardType( DatabaseRow.substring(CardNumberSequenceMatcher.start(), CardNumberSequenceMatcher.end( )));
+
+				select (thisCard ){
+
+					case Amex: result += 25;
+								pLastMatchDescription = pLastMatchDescription + "<BR>American Express Card Number";
+					case Discover: result += 25;
+								pLastMatchDescription = pLastMatchDescription + "<BR>Discover Card Number";
+					case MasterCard: results += 25;
+								pLastMatchDescription = pLastMatchDescription + "<BR>MasterCard Card Number";
+					case Visa: results += 25;
+								pLastMatchDescription = pLastMatchDescription + "<BR>Visa Card Number";
+					
+				}
+
+
 
 		}
 
