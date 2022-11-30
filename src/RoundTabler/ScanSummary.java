97e1214f3429f0922521de1b1b35dd0c6b87977e
Jordan <jmcmillan@knights.ucf.edu>
2022-11-30 13:52:10 -0500

    Consistent style for ScanSummary

--- src/RoundTabler/ScanSummary.java
+++ src/RoundTabler/ScanSummary.java
@@ -2,26 +2,31 @@ package RoundTabler;
 
 import java.util.ArrayList;
 
+/*
+ * Class/structure that contains all ScanResults
+*/
+
 public class ScanSummary{
 
 	private ArrayList<RoundTabler.ScanResult> ScannerResults = new ArrayList<RoundTabler.ScanResult>();
 
-	public ScanSummary(){
+	public ScanSummary() {
 		super();
-		}
+	}
 
-	public void addResult( RoundTabler.ScanResult NewResult) {
+	public void addResult( RoundTabler.ScanResult NewResult ) {
 		ScannerResults.add( NewResult );
 	}
 
 	public String toString() {
 		StringBuilder temp;
-		int Index;
+		int index;
 		temp = new StringBuilder();
-		for( Index=0; Index < ScannerResults.size(); Index++ ){
-			temp.append( ScannerResults.get(Index).toString() + "\n");
+		
+		for( index=0; index < ScannerResults.size(); index++ ){
+			temp.append( ScannerResults.get(index).toString() + "\n" );
 		}
+
 		return temp.toString();
 	}
-
 }
